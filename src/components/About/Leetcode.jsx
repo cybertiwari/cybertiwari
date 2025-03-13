aa3642656b46ee4f3d3cdcd44eac2dee28e1739a
Sunil Tiwari <cybertiwari@gmail.com>
2025-03-13 22:55:51 +0530

    change css to fix leetcode calender

--- src/components/About/Leetcode.jsx
+++ src/components/About/Leetcode.jsx
@@ -3,13 +3,6 @@ import LeetCodeCalendar from "leetcode-calendar";
 import { Row } from "react-bootstrap";
 
 function Leetcode() {
-	const svgElement = document.querySelector(".styles-module_calendar__sT1ND");
-	console.log(svgElement);
-
-	if (svgElement) {
-		svgElement.setAttribute("height", "0");
-	}
-
 	return (
 		<Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
 			<h1 className="project-heading" style={{ paddingBottom: "20px" }}>
@@ -22,9 +15,9 @@ function Leetcode() {
 				color="#c084f5"
 				fontSize={16}
 				width="0"
-				style={{
-					maxWidth: "1055px",
-				}}
+				// style={{
+				// 	maxWidth: "1055px",
+				// }}
 			/>
 		</Row>
 	);
